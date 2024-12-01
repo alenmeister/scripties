2f4a8fee2385db845580c93d059e32a47ae451d7
Alen Mistric <alen@mistric.no>
2024-12-01 23:50:16 +0100

    Tidy up comments

--- shell/check_smtpd.sh
+++ shell/check_smtpd.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
-#
-# $OpenBSD: check_smtpd.sh, 2018/07/12 01:23:21 anigma Exp $
 
 PROCESS=$(rcctl ls stopped | grep -w 'smtpd' | wc -l)
 
-# check if smtpd daemon is running
+# Check if the SMTPD daemon is running
 if [ $PROCESS -eq 1 ]; then
-    # create symlinks for MariaDB socket
+    # Create symlinks for the MariaDB socket
     mkdir -p /var/run/mysql
     ln -sf /var/www/var/run/mysql/mysql.sock /var/run/mysql/mysql.sock
+    
+    # Start the SMTPD daemon
     rcctl start smtpd
 fi
